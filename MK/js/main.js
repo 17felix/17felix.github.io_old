be88a97531ff99dfababe4d1ea1cbc9f88b8f295
17felix <57413284+17felix@users.noreply.github.com>
2021-03-21 12:26:21 +0300

    Update main.js
    delete poster img

--- MK/js/main.js
+++ MK/js/main.js
@@ -3,7 +3,8 @@ $(document).ready(function () {
     mp4: "https://17felix.github.io/MK/video/video.mp4",
     webm: "https://17felix.github.io/MK/video/video.webm",
     ogv: "https://17felix.github.io/MK/video/video.ogv",
-    poster: "https://17felix.github.io/MK/img/cover-image.jpg",
+    poster: "",
+//     here must be poster.img ["https://17felix.github.io/MK/img/cover-image.jpg"], but i have some troubles to enable it in right way
   });
 });
 
